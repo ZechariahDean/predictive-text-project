c6b5bbce275e246ce97bc7374996d3e2f5abfb57
zenithalegend <zeby11@ymail.com>
2024-06-07 16:51:18 -0400

    add all completed files to repo

---
+++ script.js
@@ -0,0 +1,44 @@
+const input = document.querySelector('#fruit');
+const suggestions = document.querySelector('.suggestions ul');
+
+const fruit = ['Apple', 'Apricot', 'Avocado 🥑', 'Banana', 'Bilberry', 'Blackberry', 'Blackcurrant', 'Blueberry', 'Boysenberry', 'Currant', 'Cherry', 'Coconut', 'Cranberry', 'Cucumber', 'Custard apple', 'Damson', 'Date', 'Dragonfruit', 'Durian', 'Elderberry', 'Feijoa', 'Fig', 'Gooseberry', 'Grape', 'Raisin', 'Grapefruit', 'Guava', 'Honeyberry', 'Huckleberry', 'Jabuticaba', 'Jackfruit', 'Jambul', 'Juniper berry', 'Kiwifruit', 'Kumquat', 'Lemon', 'Lime', 'Loquat', 'Longan', 'Lychee', 'Mango', 'Mangosteen', 'Marionberry', 'Melon', 'Cantaloupe', 'Honeydew', 'Watermelon', 'Miracle fruit', 'Mulberry', 'Nectarine', 'Nance', 'Olive', 'Orange', 'Clementine', 'Mandarine', 'Tangerine', 'Papaya', 'Passionfruit', 'Peach', 'Pear', 'Persimmon', 'Plantain', 'Plum', 'Pineapple', 'Pomegranate', 'Pomelo', 'Quince', 'Raspberry', 'Salmonberry', 'Rambutan', 'Redcurrant', 'Salak', 'Satsuma', 'Soursop', 'Star fruit', 'Strawberry', 'Tamarillo', 'Tamarind', 'Yuzu'];
+
+function search(str) {
+	let results = [];
+
+	for (let item of fruit) {
+		if (item.toLowerCase().includes(str) &&  results.length < 6)
+			results.push(item);
+	};
+
+	return results;
+}
+
+function searchHandler(e) {
+	suggestions.innerHTML = '';
+
+	const curText = input.value.toLowerCase();
+	showSuggestions(search(curText));
+	
+
+	if (input.value === '') suggestions.innerHTML = '';
+}
+
+function showSuggestions(results) {
+	results.forEach(item => {
+		const newLi = document.createElement('li');
+		newLi.innerText = item;
+		suggestions.appendChild(newLi);
+	});
+	
+}
+
+function useSuggestion(e) {
+	const result = e.target;
+	input.value = result.innerText;
+
+	suggestions.innerHTML = '';
+}
+
+input.addEventListener('keyup', searchHandler);
+suggestions.addEventListener('click', useSuggestion);
